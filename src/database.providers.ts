8227fe2a524185c2c8e8cdd35a443a461680440f
Overraddit <kenwooshim@gmail.com>
2023-04-17 20:59:56 +0900

    Fix: modify db host

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -13,7 +13,7 @@ export const databaseProviders = [
       const dataSource = new DataSource({
         name: 'default',
         type: 'postgres',
-        host: 'localhost',
+        host: 'postgres',
         port: 5432,
         username: 'postgres',
         password: '1234',
