cbfe6419e5ee32cd5ebe476c7bd4922129b23290
Overraddit <kenwooshim@gmail.com>
2023-04-17 22:40:36 +0900

    Revert "merge : docker"
    This reverts commit d2e947f05fa5f2a73f031c18788528c24a1fb3da.

--- src/cookie/cookie.service.ts
+++ src/cookie/cookie.service.ts
@@ -5,10 +5,7 @@ type cookieType = string | undefined;
 @Injectable()
 export class CookieService {
   extractCookie(cookieRaw: string): cookieType {
-    let cookie = cookieRaw.split('; ')[1];
-    if (cookie == undefined) return undefined;
-    if (cookie.startsWith('session_key=') == false) return undefined;
-    cookie = cookie.split('=')[1];
-    return cookie;
+    if (cookieRaw == undefined) return undefined;
+    return cookieRaw;
   }
 }
