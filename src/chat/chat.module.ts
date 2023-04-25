811f577796eb30cf1a0a14ab6c73d9121d853d49
Overraddit <kenwooshim@gmail.com>
2023-04-25 21:54:19 +0900

    Feat: add ChatGateway in ChatModule

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -4,11 +4,12 @@ import { ChatService } from './chat.service';
 import { chatProviders } from './chat.providers';
 import { DatabaseModule } from 'src/database.module';
 import { UserModule } from 'src/user/user.module';
+import { ChatGateway } from 'src/events/chat.gateway';
 
 @Module({
   imports: [DatabaseModule, UserModule],
   controllers: [ChatController],
-  providers: [ChatService, ...chatProviders],
+  providers: [ChatGateway, ChatService, ...chatProviders],
   exports: [ChatService],
 })
 export class ChatModule {}
