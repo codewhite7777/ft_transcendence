d06572b01f27e4c87bd8703138cf84c55aad05a4
alee <alee@aleeui-MacBookPro.local>
2023-05-05 20:25:55 +0900

    feat : directory static

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { SocketParameterValidationExceptionFilter } from './events/exceptionFilter';
+import * as path from 'path';
+import * as express from 'express';
+import * as serveStatic from 'serve-static';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +16,8 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
   });
+  const uploadsPath = path.resolve(__dirname, 'uploads');
+  app.use('/uploads', express.static(uploadsPath));
   await app.listen(3000);
 }
 bootstrap();
