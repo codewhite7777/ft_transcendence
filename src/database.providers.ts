93a3f357dec8f6ec784c065657f84bcbbec7f816
JeonYoungHo-youjeon <youjeon@student.42seoul.kr>
2023-05-07 17:24:37 +0900

    wip

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -14,7 +14,7 @@ export const databaseProviders = [
       const dataSource = new DataSource({
         name: 'default',
         type: 'postgres',
-        host: 'postgres',
+        host: 'localhost',
         port: 5432,
         username: 'postgres',
         password: '1234',
